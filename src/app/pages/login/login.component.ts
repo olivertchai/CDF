22d01429e0983774c60b1d2322a37348bf452c8b
Wagner Camargo Oliveira <32493077+olivertchai@users.noreply.github.com>
2025-06-18 01:27:50 -0300

    Add files via upload

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { NgModel } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-login',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  login = {
+    email: '',
+    password: ''
+  };
+
+  erroLogin: boolean = false;
+
+  constructor(private router: Router) {}
+
+  onLogin() {
+
+    const cadastros = JSON.parse(localStorage.getItem('cadastros') || '[]');
+
+    const usuarioValido = cadastros.find((usuario: any) => {
+      return usuario.email === this.login.email && usuario.password === this.login.password;
+    });
+
+    if (usuarioValido) {
+      console.log('Login bem-sucedido!');
+      this.erroLogin = false;
+      localStorage.setItem('usuarioLogado', JSON.stringify(usuarioValido));
+      this.router.navigate(['']); 
+    } else {
+      this.erroLogin = true;
+      console.log('Email ou senha inválidos.');
+    }
+  }
+
+  isInvalid(control: any): boolean {
+    return !!(control?.invalid && control?.touched);
+  }
+
 
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,15 +1,39 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { FooterComponent } from "../../components/footer/footer.component";
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-register',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent,FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  register = {
+    name: '',
+    surname: '',
+    email: '',
+    password: '',
+    dataCadastro:''
+  };
+
+  constructor(private router: Router) {}
+
+    onSubmit(){         // o parse serve para converter o que é string em um object JS
+      const cadastros = JSON.parse(localStorage.getItem('cadastros')|| '[]');
+
+      cadastros.push(this.register);
+      this.register.dataCadastro = new Date().toISOString(); 
+
+
+      localStorage.setItem('registers', JSON.stringify(this.register))
+
+      this.router.navigate(['/login']);
+
+    }
+
 
 }
